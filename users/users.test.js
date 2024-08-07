8f9179ae63e4474e7c7c66219558295b564dfc64
ajbecks <aaron_beckles@hotmail.co.uk>
2024-08-07 15:40:49 +0100

    Ticket 4 done

--- users/users.test.js
+++ users/users.test.js
@@ -1,5 +1,11 @@
 import {test, expect} from "vitest";
+import request from "supertest";
+import app from "../app.js";
 
-test('GET /api/health works', () => {
-    //expect(calculateScrabbleScore('')).toBe(1);
+test('GET /api/health works', async () => {
+    const response = await request(app).get('/api/health');
+    expect(response.body).toEqual ({
+        success: true,
+        payload: "API is running correctly",
+  });
   });
