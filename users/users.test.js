9536ba205175d9826628be55976cfe0dcf66e3f1
Adil <adilk23@outlook.com>
2024-08-07 15:54:09 +0100

    complete assertion 1 and assertion 2 of ticket 5

--- users/users.test.js
+++ users/users.test.js
@@ -7,5 +7,16 @@ test('GET /api/health works', async () => {
     expect(response.body).toEqual ({
         success: true,
         payload: "API is running correctly",
+   
   });
+    expect(response.status).toEqual(200);
+    expect(response.headers["Content-Type"]).toMatch(/json/)
   });
+
+
+
+  // Assertion 1 - response body is object with correct structure ✅
+
+  // Assertion 2  - response status code of 200 ✅
+
+  // Assertion 3 - content-Type response header contains application/json. 
